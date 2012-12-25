7deeaaf9c6e13ffebdb5c9bc1deb9461ceb54a22
maksa <milan.maksimovic@gmail.com>
2012-12-25 23:09:04 +0100

    Updated original author

--- Polumentizator.m
+++ Polumentizator.m
@@ -1,9 +1,10 @@
 //
 //  Polumentizator.m
 //  Polumenta
-//
 //  Created by Milan Maksimovic on 12/25/12.
-//  Copyright (c) 2012 maksa. All rights reserved.
+
+//  Based on polumenta generator by Vladimir Mitrovic
+//  https://github.com/vl4dimir/polumenta
 //
 
 #import "Polumentizator.h"
